cbb21e4dfdd91406cf0bf071150b4e84eccccb41
Deepak Bhattarai <bring2dip@gmail.com>
2020-01-11 09:30:46 +0545

    log to stdout only. bump version

--- server/bunyan-logger.js
+++ server/bunyan-logger.js
@@ -1,31 +1,15 @@
 'use strict';
 
+const path = require('path');
+const config = require('./config');
+const mainFile = path.basename(require.main.filename);
 
-const path = require('path'),
-    config = require('./config'),
-    mainFile = path.basename(require.main.filename);
-
-//change log date every time app restarts
-//clear file using echo "" > log-file-name
-//in the format YYYY-MM-DD-mm
 const now = new Date();
 const logStartDate = `${now.getFullYear()}-${(now.getMonth() + 1)}-${now.getDate()}`;
 const loggerName = `logs-${logStartDate}`;
-let streams = null;
-if (config.isProd && config.appEngine !== 'ROFS') {
-  streams = [{path: 'log/' + loggerName + '.log'}];
-} /*else if (process.env.NODE_ENV === 'test') {
-  streams = [{
-    path: 'log/' + loggerName + '.log'
-  }, {
-    stream: process.stdout
-  }];
-} */ else {
-  // if running in Development mode or if App Engine runs on read only file system
-  streams = [{stream: process.stdout}];
-}
+let streams = [{ stream: process.stdout }];
 
 module.exports = require('bunyan').createLogger({
   name: loggerName,
-  streams: streams
+  streams: streams,
 });
